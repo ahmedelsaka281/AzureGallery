389d21845fde7a5ec9e51bb3a2dce4e7a6e71df0
AhmEd ElsaKa <AhmEd ElsaKa@XPROG>
2019-08-03 14:43:22 +0200

    .

--- AzureGallery-SPA/src/app/_interceptors/token.interceptor.ts
+++ AzureGallery-SPA/src/app/_interceptors/token.interceptor.ts
@@ -7,6 +7,7 @@ import { HttpInterceptor, HttpRequest, HttpHandler, HttpEvent } from '@angular/c
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
     constructor(public auth: AuthService) { }
+
     intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
         request = request.clone({
             setHeaders: {
